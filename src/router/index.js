478cccc298c7846a953b967907aabc0c3de24d4f
wer9808 <wer9808@naver.com>
2026-04-08 10:29:07 +0900

    feat: 프로젝트 기본 구조 및 라우터 페이지 작성

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,38 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import "@/assets/main.css";
+
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "@/views/Home.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+  routes: [
+    {
+      path: "/",
+      component: Home,
+    },
+    {
+      path: "/auth",
+      children: [
+        { path: "login", component: import("@/views/auth/Login.vue") },
+        { path: "signup", component: import("@/views/auth/Signup.vue") },
+      ],
+    },
+    {
+      path: "/ledger",
+      children: [
+        {
+          path: "dashboard",
+          component: import("@/views/ledger/Dashboard.vue"),
+        },
+        { path: "mypage", component: import("@/views/ledger/MyPage.vue") },
+        {
+          path: "transactions",
+          component: import("@/views/ledger/Transactions.vue"),
+        },
+        { path: "stat", component: import("@/views/ledger/Statistic.vue") },
+      ],
+    },
+  ],
+});
 
-export default router
+export default router;
