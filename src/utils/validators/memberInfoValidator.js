4b0dfb40acb0a53e4a74ce8d544119bc1d67c9bc
wer9808 <60593364+wer9808@users.noreply.github.com>
2026-04-09 12:48:54 +0900

    Feat. 회원정보 규칙 검사 모듈 구현 (#42)
    * feat: 로그인 및 회원가입 관련 에러코드 추가
    
    * feat: 멤버 정보 Validator 구현
    
    * fix: validator 디렉토리명 변경

---
+++ src/utils/validators/memberInfoValidator.js
@@ -0,0 +1,63 @@
+import { ErrorCode } from '@/constant/errorCode';
+
+export class MemberInfoError extends Error {
+  constructor(errorInfo) {
+    super(errorInfo.msg);
+    this.name = 'MemberInfoError';
+    this.code = errorInfo.code;
+  }
+}
+
+// 정규식 정의
+const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const PASSWORD_REGEX = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]).{8,20}$/;
+const NAME_REGEX = /^[가-힣a-zA-Z\s]+$/;
+
+/**
+ * 이메일 유효성 검사
+ */
+export function isEmailValid(email) {
+  if (!email) {
+    throw new MemberInfoError(ErrorCode.EMPTY_EMAIL_FIELD);
+  }
+  if (!EMAIL_REGEX.test(email)) {
+    throw new MemberInfoError(ErrorCode.INVALID_EMAIL_FORMAT);
+  }
+  return true;
+}
+
+/**
+ * 비밀번호 유효성 검사
+ */
+export function isPasswordValid(password) {
+  if (!password) {
+    throw new MemberInfoError(ErrorCode.EMPTY_PASSWORD_FIELD);
+  }
+  if (!PASSWORD_REGEX.test(password)) {
+    throw new MemberInfoError(ErrorCode.INVALID_PASSWORD_FORMAT);
+  }
+  return true;
+}
+
+/**
+ * 비밀번호 확인 일치 검사
+ */
+export function isPasswordConfirmed(password, confirmPassword) {
+  if (password !== confirmPassword) {
+    throw new MemberInfoError(ErrorCode.PASSWORD_MISMATCH);
+  }
+  return true;
+}
+
+/**
+ * 이름 유효성 검사
+ */
+export function isNameValid(name) {
+  if (!name) {
+    throw new MemberInfoError(ErrorCode.EMPTY_NAME_FIELD);
+  }
+  if (!NAME_REGEX.test(name)) {
+    throw new MemberInfoError(ErrorCode.INVALID_NAME_FORMAT);
+  }
+  return true;
+}
