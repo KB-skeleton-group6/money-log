85bd491ce00aefabfc2b37b88d1fa8045b88b9be
하헌찬 <127941120+HaHeonchan@users.noreply.github.com>
2026-04-09 14:04:40 +0900

    Revert "feat: 가계부 대시보드 페이지 UI 구성"

--- src/router/index.js
+++ src/router/index.js
@@ -1,46 +1,8 @@
-import "@/assets/main.css";
-
-import { createRouter, createWebHistory } from "vue-router";
-import Home from "@/views/Home.vue";
+import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      component: Home,
-    },
-    {
-      path: "/auth",
-      children: [
-        { path: "login", component: () => import("@/views/auth/Login.vue") },
-        { path: "signup", component: () => import("@/views/auth/Signup.vue") },
-      ],
-    },
-    {
-      path: "/ledger",
-      component: () => import("@/layouts/LedgerLayout.vue"),
-      redirect: "/ledger/dashboard",
-      children: [
-        {
-          path: "dashboard",
-          component: () => import("@/views/ledger/Dashboard.vue"),
-        },
-        {
-          path: "mypage",
-          component: () => import("@/views/ledger/MyPage.vue"),
-        },
-        {
-          path: "transactions",
-          component: () => import("@/views/ledger/Transactions.vue"),
-        },
-        {
-          path: "stat",
-          component: () => import("@/views/ledger/Statistic.vue"),
-        },
-      ],
-    },
-  ],
-});
+  routes: [],
+})
 
-export default router;
+export default router
