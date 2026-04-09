92dbf1041b5127ef8411b2198b7cbf082543d95c
wer9808 <wer9808@naver.com>
2026-04-09 10:18:51 +0900

    feat: axios 클라이언트 라이브러리 구현 및 프록시 적용

---
+++ src/api/axiosClient.js
@@ -0,0 +1,167 @@
+import axios from "axios";
+import {
+  FetchFailedError,
+  NetworkOfflineError,
+  SaveFailedError,
+} from "./networkError";
+import { ErrorCode } from "@/constant/errorCode";
+
+const urlPrefix = "/api";
+
+const isRequestSuccessful = (statusCode) => {
+  return statusCode >= 200 && statusCode < 300;
+};
+
+const onFetchFailed = () => {
+  throw new FetchFailedError();
+};
+
+const onSaveFailed = () => {
+  throw new SaveFailedError();
+};
+
+const onNetworkOffline = () => {
+  throw new NetworkOfflineError();
+};
+
+const checkRequestFailed = (statusCode, errorCode) => {
+  if (!isRequestSuccessful(statusCode)) {
+    switch (errorCode.code) {
+      case ErrorCode.FETCH_FAILED.code:
+        onFetchFailed();
+        break;
+      case ErrorCode.SAVE_FAILED.code:
+        onSaveFailed();
+        break;
+      default:
+        onNetworkOffline();
+    }
+  }
+};
+
+async function getMembers() {
+  const res = await axios.get(`${urlPrefix}/members`);
+
+  checkRequestFailed(res.status, ErrorCode.FETCH_FAILED);
+
+  const members = res.data;
+
+  return members;
+}
+
+async function getMember(memberId) {
+  const res = await axios.get(`${urlPrefix}/members/${memberId}`);
+
+  checkRequestFailed(res.status, ErrorCode.FETCH_FAILED);
+
+  const member = res.data;
+
+  return member;
+}
+
+async function createMember(member) {
+  const res = await axios.post(`${urlPrefix}/members`, member);
+
+  checkFetchFailed(res.status, ErrorCode.SAVE_FAILED);
+
+  const createdMember = res.data;
+
+  return createdMember;
+}
+
+async function updateMember(memberId, member) {
+  const res = await axios.put(`${urlPrefix}/members/${memberId}`, member);
+
+  checkFetchFailed(res.status, ErrorCode.SAVE_FAILED);
+
+  const updatedMember = res.data;
+
+  return updatedMember;
+}
+
+async function deleteMember(memberId) {
+  const res = await axios.delete(`${urlPrefix}/members/${memberId}`);
+
+  checkFetchFailed(res.status, ErrorCode.SAVE_FAILED);
+}
+
+async function getTransactions() {
+  const res = await axios.get(`${urlPrefix}/transactions`);
+
+  checkRequestFailed(res.status, ErrorCode.FETCH_FAILED);
+
+  const transactions = res.data;
+
+  return transactions;
+}
+
+async function getTransaction(transactionId) {
+  const res = await axios.get(`${urlPrefix}/transactions/${transactionId}`);
+
+  checkRequestFailed(res.status, ErrorCode.FETCH_FAILED);
+
+  const transaction = res.data;
+
+  return transaction;
+}
+
+async function createTransaction(transaction) {
+  const res = await axios.post(`${urlPrefix}/transactions`, transaction);
+
+  checkFetchFailed(res.status, ErrorCode.SAVE_FAILED);
+
+  const createdTransaction = res.data;
+
+  return createdTransaction;
+}
+
+async function updateTransaction(transactionId, transaction) {
+  const res = await axios.put(
+    `${urlPrefix}/transactions/${transactionId}`,
+    transaction,
+  );
+
+  checkFetchFailed(res.status, ErrorCode.SAVE_FAILED);
+
+  const updatedTransaction = res.data;
+
+  return updatedTransaction;
+}
+
+async function deleteTransaction(transactionId) {
+  const res = await axios.delete(`${urlPrefix}/transactions/${transactionId}`);
+
+  checkFetchFailed(res.status, ErrorCode.SAVE_FAILED);
+}
+
+async function getCategories() {
+  const res = await axios.get(`${urlPrefix}/categories`);
+
+  checkRequestFailed(res.status, ErrorCode.FETCH_FAILED);
+
+  const categories = res.data;
+
+  return categories;
+}
+
+const memberApi = {
+  getMembers,
+  getMember,
+  createMember,
+  updateMember,
+  deleteMember,
+};
+
+const transactionApi = {
+  getTransactions,
+  getTransaction,
+  createTransaction,
+  updateTransaction,
+  deleteTransaction,
+};
+
+const categoryApi = {
+  getCategories,
+};
+
+export { memberApi, transactionApi, categoryApi };
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,26 @@
-import { fileURLToPath, URL } from 'node:url';
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import vueDevTools from 'vite-plugin-vue-devtools';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
-  base: '/money-log/',
+  base: "/money-log/",
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  server: {
+    port: 8080,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
     },
   },
 });
